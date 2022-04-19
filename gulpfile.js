73ebaec122fdad575297bf47ca29e077b22eb934
CarlAbel <caweb.cph@gmail.com>
2022-04-19 10:54:13 +0200

    update - sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -14,24 +14,20 @@ const sass = gulpSass(tempSass)
 
 function html() {
     return gulp.src("src/html/*.html")
-        .pipe(sourcemaps.init())
         .pipe(include())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest("build"))
         .pipe(connect.reload())
 }
 
 function buildHtml() {
     return gulp.src("src/html/*.html")
         .pipe(include())
-        .pipe(gulp.dest("build"))      
+        .pipe(gulp.dest("build"))
 }
 
 function css() {
     return gulp.src("src/styles/*.scss")
-        .pipe(sourcemaps.init())
         .pipe(sass().on("error", sass.logError))
-        .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("build/styles"))
         .pipe(connect.reload())
 }
@@ -39,12 +35,12 @@ function css() {
 function buildCss() {
     return gulp.src("src/styles/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("build/styles"))      
+        .pipe(gulp.dest("build/styles"))
 }
 
 function js() {
     return gulp.src("src/js/*.js")
-        .pipe(sourcemaps.init())
+
         .pipe(concat("app.js"))
         .pipe(uglifyjs())
         .pipe(sourcemaps.write("."))
@@ -53,7 +49,7 @@ function js() {
 }
 
 function buildJs() {
-    return gulp.src("src/js/*.js")  
+    return gulp.src("src/js/*.js")
         .pipe(concat("app.js"))
         .pipe(uglifyjs())
         .pipe(gulp.dest("build/js"))
@@ -73,25 +69,25 @@ function buildImages() {
 }
 
 function watchImages() {
-    gulp.watch("src/images/*", {events:"all",ignoreInitial:false}, async function() {
+    gulp.watch("src/images/*", { events: "all", ignoreInitial: false }, async function () {
         images()
     })
 }
 
 function watchCss() {
-    gulp.watch("src/styles/*.scss", {events:"all",ignoreInitial:false}, async function() {
+    gulp.watch("src/styles/*.scss", { events: "all", ignoreInitial: false }, async function () {
         css()
     })
 }
 
 function watchJs() {
-    gulp.watch("src/js/*.js", {events:"all",ignoreInitial:false}, async function() {
+    gulp.watch("src/js/*.js", { events: "all", ignoreInitial: false }, async function () {
         js()
     })
 }
 
 function watchHtml() {
-    gulp.watch("src/html/*.html", {events:"all",ignoreInitial:false}, function(cb) {
+    gulp.watch("src/html/*.html", { events: "all", ignoreInitial: false }, function (cb) {
         html()
         cb()
     })
@@ -104,7 +100,7 @@ function server() {
     })
 }
 
-export const watcher = gulp.parallel(watchHtml, watchCss, watchImages,  watchJs)
+export const watcher = gulp.parallel(watchHtml, watchCss, watchImages, watchJs)
 export { css, js, images, html }
 export default gulp.parallel(watcher, server)
 export const build = gulp.parallel(buildHtml, buildCss, buildJs, buildImages)
